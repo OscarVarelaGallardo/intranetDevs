d6008d24d5314372fb44bec400f17fa65db81f67
kevin-fernandez-escotel <126614439+kevin-fernandez-escotel@users.noreply.github.com>
2023-03-16 12:36:24 -0600

    Commit-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-
-import './App.css'
+import { useEffect, useState } from 'react';
+import Comunicados from './Comunicados/Comunicados';
+import Loading from './Helpers/Loading';
 
 function App() {
-  
+  const [comunicados, setComunicados] = useState([])
 
-  return (
+  const peticion = async () => {
+    const response = await fetch('http://192.168.9.41:5000/api/GetComunicadosActivos', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   
-    <>
-    <h1>hola</h1>
-    </>
-  )
+    const data = await response.json();
+  
+    setComunicados(data);
+  }
+
+  useEffect(() => {
+    peticion()
+  }, [])
+
+  {
+    return comunicados.length > 0 ? <Comunicados comunicados={comunicados}/> : <Loading/>
+  }
 }
 
 export default App
